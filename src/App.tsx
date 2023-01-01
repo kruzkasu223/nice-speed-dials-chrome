9199e59667968d9e1b9ff1db35d96466194a955d
Krushang Kasundra <kruzkasu223@gmail.com>
2023-01-01 18:19:19 +0530

    feat(package): added hope ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { type Component } from 'solid-js'
 import { Grid } from './components'
 import { app } from './styles'
+import { HopeProvider } from '@hope-ui/core'
 
 const App: Component = () => {
   return (
-    <div class={app}>
-      <Grid />
-    </div>
+    <HopeProvider initialColorMode="dark">
+      <div class={app}>
+        <Grid />
+      </div>
+    </HopeProvider>
   )
 }
 
