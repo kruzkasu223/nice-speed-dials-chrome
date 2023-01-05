93a94d03a82b94d42b66c2ced5df8b97e6a907f2
Krushang Kasundra <kruzkasu223@gmail.com>
2023-01-05 21:39:51 +0530

    feat(theme): added custom theme colours

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { type Component } from 'solid-js'
 import { Grid } from './components'
 import { app } from './styles'
-import { HopeProvider } from '@hope-ui/core'
+import { createPalette, extendTheme, HopeProvider } from '@hope-ui/core'
+
+const theme = extendTheme({
+  colors: {
+    dark: {
+      primary: createPalette({
+        50: '#fff0f4',
+        100: '#fedce5',
+        200: '#febed0',
+        300: '#fd91b0',
+        400: '#fa618c',
+        500: '#f63c71',
+        600: '#eb245c',
+        700: '#d71d52',
+        800: '#ae1e47',
+        900: '#8a1e3d',
+      }),
+    },
+  },
+})
 
 const App: Component = () => {
   return (
-    <HopeProvider initialColorMode="dark">
+    <HopeProvider initialColorMode="dark" theme={theme}>
       <div class={app}>
         <Grid />
       </div>
